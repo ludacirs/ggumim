ec81084af298cfb441feacacedab904be3744636
ludacirs <ludacirs@naver.com>
2022-02-02 03:02:57 +0900

    chore: assets 파일 추가, alias 경로 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,6 +9,7 @@ module.exports = {
       ...config.resolve.alias,
       "@styles": path.resolve(__dirname, "../src/styles/"),
       "@components": path.resolve(__dirname, "../src/components/"),
+      "@assets": path.resolve(__dirname, "../src/assets/"),
     };
     return config;
   },
