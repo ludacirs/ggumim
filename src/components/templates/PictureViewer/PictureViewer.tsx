432baefb689c6b86d656a1b051e3c25587a7520a
ludacirs <ludacirs@naver.com>
2022-02-02 14:24:10 +0900

    chore: 디렉토리 변경
    molecules, organisms, templates 폴더 추가

---
+++ src/components/templates/PictureViewer/PictureViewer.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const PictureViewerBlock = styled.div``;
+
+interface PictureViewerProps {}
+
+const PictureViewer = ({}: PictureViewerProps) => {
+    return (
+        <PictureViewerBlock>
+
+        </PictureViewerBlock>
+    );
+};
+
+export default PictureViewer;
---
+++ src/components/templates/index.ts
@@ -0,0 +1,1 @@
+export { default as PictureViewer } from "./PictureViewer/PictureViewer";
