2e833fba11373e41025413a2df52161a1404bfe3
ludacirs <ludacirs@naver.com>
2022-02-03 03:14:26 +0900

    feat: slide 오토 스크롤 구현
    선택된 상품으로 자동 스크롤

--- src/components/organisms/ImageSlide/ImageSlide.tsx
+++ src/components/organisms/ImageSlide/ImageSlide.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { IProduct } from "@models/ImageView";
 import { ImageBox } from "@components/atoms";
 import { useProductState, useSetProduct } from "@contexts/ProductContext";
+import { useEffect, useRef } from "react";
 
 const ImageSlideBlock = styled.div`
   display: flex;
@@ -35,6 +36,8 @@ interface ImageSlideProps {
 const ImageSlide = ({ productList }: ImageSlideProps) => {
   const setProduct = useSetProduct();
   const selectedProduct = useProductState();
+  const slideItemRef = useRef<{ [propsName: string]: HTMLDivElement }>({});
+  const sliderRef = useRef<any>();
 
   const toggleTooltip = (nextProduct: string) => {
     return () => {
@@ -46,11 +49,25 @@ const ImageSlide = ({ productList }: ImageSlideProps) => {
     };
   };
 
+  useEffect(() => {
+    const imageElems = [...document.querySelectorAll(".slide-item")] as HTMLDivElement[];
+
+    slideItemRef.current = imageElems.reduce((map, $elem) => {
+      map[$elem.dataset.name!] = $elem;
+      return map;
+    }, slideItemRef.current);
+  }, []);
+
+  useEffect(() => {
+    const x = slideItemRef.current[selectedProduct]?.getBoundingClientRect().x;
+    sliderRef.current.scrollTo(x, 0);
+  }, [selectedProduct]);
+
   return (
-    <ImageSlideBlock>
+    <ImageSlideBlock ref={sliderRef}>
       <SlideWrapper>
         {productList.map(({ productId, imageUrl, discountRate, productName }) => (
-          <SlideItem key={productId}>
+          <SlideItem key={productId} className={"slide-item"} data-name={productName}>
             <ImageBox
               imageUrl={imageUrl}
               selected={selectedProduct === productName}
